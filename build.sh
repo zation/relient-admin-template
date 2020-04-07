9e906ad11062895c6520248123a7064b7f3f759e
Zation <zation1@gmail.com>
2020-04-07 20:32:31 +0800

    improve build

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-REPOSITORY=registry.cn-hangzhou.aliyuncs.com/boda/admin
+REPOSITORY=registry.cn-hangzhou.aliyuncs.com/repo/project
 CI_BUILD_TAG=${1:-latest}
 ENV=${2:-production}
-CONTAINER=boda-admin
+CONTAINER=container
 
 yarn
 NODE_ENV=production npm run build
 docker build -t ${REPOSITORY}:${CI_BUILD_TAG} .
 docker push ${REPOSITORY}:${CI_BUILD_TAG}
-# docker stop ${CONTAINER}
-# docker rm ${CONTAINER}
-# docker run -e NODE_ENV=${ENV} --name ${CONTAINER} -d -p 3100:3100 --restart always ${REPOSITORY}:${CI_BUILD_TAG}
 git tag ${CI_BUILD_TAG}
 git push origin --tags
