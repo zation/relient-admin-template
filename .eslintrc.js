24a6e3e6f92ba184b758bd0b6a9fa410f83642bf
Liu Yang <zation1@gmail.com>
2021-04-21 13:40:42 +0800

    Revert "add ts config"
    This reverts commit a7160dd6c5694c30a8289c94830a54f51a2b9982.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,18 @@
 // ESLint configuration
 // http://eslint.org/docs/user-guide/configuring
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: 'babel-eslint',
 
-  plugins: [
-    '@typescript-eslint',
-    'css-modules',
+  extends: [
+    'airbnb',
+    'plugin:css-modules/recommended',
   ],
+
+  plugins: ['css-modules'],
 
   globals: {
     __DEV__: true,
     __BROWSER__: true,
-  },
-
-  extends: [
-    'airbnb-typescript',
-    'plugin:css-modules/recommended',
-  ],
-
-  parserOptions: {
-    project: './tsconfig.json',
   },
 
   env: {
@@ -76,7 +69,7 @@ module.exports = {
 
     // Allow .js files to use JSX syntax
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
-    'react/jsx-filename-extension': ['error', { extensions: ['.ts', '.tsx'] }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
 
     // Functional and class components are equivalent from React’s point of view
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md
@@ -86,7 +79,6 @@ module.exports = {
     'object-curly-newline': 'off',
     'react/forbid-prop-types': 'off',
     'react/require-default-props': 'off',
-    'max-len': ['error', { 'code': 120 }],
   },
 
   settings: {
--- src/client/store.js
+++ src/client/store.js
@@ -6,14 +6,13 @@ import {
 import reducers from 'shared/reducers';
 import fetch from 'isomorphic-fetch/fetch-npm-browserify';
 import fetchMiddleware from 'shared/middlewares/fetch';
-import { message } from 'antd';
+import { Message } from 'antd';
 import { prop, flow, first } from 'lodash/fp';
 import pushMiddleware from 'relient-admin/middlewares/push';
 import getConfig from 'relient/config';
 import authorization from './middlewares/cookie';
 import history from './history';
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
 const { __REDUX_DEVTOOLS_EXTENSION__, __INITIAL_STATE__ = {} } = global;
 
 const middlewares = [
@@ -23,13 +22,13 @@ const middlewares = [
   historyMiddleware(history),
   serverError({
     onUnauthorized: () => {
-      message.error('权限错误，请重新登陆适当账号', 5);
+      Message.error('权限错误，请重新登陆适当账号', 5);
     },
     onGlobalWarning: async ({ payload }) => {
       if (payload instanceof Array) {
-        message.error(flow(first, prop('message'))(payload), 5);
+        Message.error(flow(first, prop('message'))(payload), 5);
       } else {
-        message.error(prop('message')(payload) || prop('error')(payload), 5);
+        Message.error(prop('message')(payload) || prop('error')(payload), 5);
       }
     },
   }),
